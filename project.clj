0b743c35e193d4acbefc844aaf156b14b36b6f65
Robert Boone <robert.l.boone@gmail.com>
2012-04-13 13:15:22 -0500

    Stop using mongodb and persist with json

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.xml "0.0.3"]
                  [org.clojure/data.json "0.1.2"]
-                 [congomongo "0.1.8"]
                  [org.clojars.doo/postal "1.8-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
   :main perl-rss.core)
--- src/perl_rss/core.clj
+++ src/perl_rss/core.clj
@@ -1,39 +1,21 @@
 (ns perl-rss.core
+  (:gen-class)
   (:use [clojure.java.io]
         [clojure.core]
         [perl-rss.modules])
-  (:require [somnium.congomongo :as db]
-            [clojure.data.json :as json]
+  (:require [clojure.data.json :as json]
             [postal.core :as mail]
             [clojure.string :as str]))
 
-(def db (db/make-connection "perlrss" :host "127.0.0.1"))
-(db/set-connection! db)
 (def settings (json/read-json (reader (file "settings.json"))))
 (def modules (json/read-json (reader (file "modules.json"))))
+(def seen (json/read-json (reader (file "seen.json"))))
+(def emails (json/read-json (reader (file "emails.json"))))
 
 (defn find-updated-modules []
   (first (filter not-empty (map find-perl-module modules))))
 
 (def updated-modules (find-updated-modules))
-
-(defn seen-modules [module-map]
-  (let [name (:name module-map)
-        version (:version module-map)]
-    (first (db/with-mongo db
-             (db/fetch :seen
-                       :where {:name name
-                               :version version})))))
-
-(defn unseen-modules []
-  (filter #(if (empty? (seen-modules %))
-             true false)
-          updated-modules))
-
-(defn update-database [& maps]
-  (db/with-mongo db
-    (map (fn [m] (db/insert! :seen {:name (:name m)
-                                    :version (:version m)})) maps)))
 
 (defn mail-message [modules]
   (map (fn [m]
@@ -42,10 +24,6 @@
                matcher (re-matcher #"_" version)
                dev (re-find matcher)]
            (str (format "%s-%s" name version) (if dev " DEVELEMENT VERSION\n" "")))) modules))
-
-(defn reset-seen []
-  (db/with-mongo db
-    (db/drop-coll! "seen")))
 
 (defn send-email [email body]
   (mail/send-message {:from (:username settings)
@@ -60,16 +38,9 @@
 
 (defn send-all-module-email [u]
   (map #(send-module-email % u)
-       (map :email
-              (db/with-mongo db
-                (db/fetch :emails)))))
-
-(defn clean-up [u]
-  (when (not-empty u)
-    (apply update-database u)))
+       emails))
 
 (defn -main [& args]
-  (let [unseen (unseen-modules)]
-       (do
-        (clean-up unseen)
-        (send-all-module-email unseen))))
+  (send-all-module-email updated-modules)
+  (let [update-seen (drop-while empty? (apply conj seen updated-modules))]
+    (spit "seen.json" (json/json-str updated-modules))))
